b722432e910a917da07eeeab8fed8df1fbe0b2e5
VB <git@github.com:VodkaBottle/ENPM611-grade-calculator.git>
2023-11-24 18:26:53 -0500

    cleaned up crash tests

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -15,6 +15,26 @@ class QuizzesTest(unittest.TestCase):
         """
         self.assertTrue(True, 'Example assertion.')
         
+    def test_crash_invalid_operations(self):
+        """
+        Each of these conditions will cause a crash in the quizzes module due to an invalid operation as a result of an unexpected argument being passed in.
+        e.g. addition is not valid between dictionaries and strings. Line 63 of quizzes_controller.py
+        """
+
+        quiz_id = self.ctrl.add_quiz({"title": "title", "text": "quiz description", "available_date": 1, "hello": 2}, "quiz description", 1, 2)
+        quiz_id = self.ctrl.add_quiz(1, "quiz description", 1, 2)
+        quiz_id = self.ctrl.add_quiz(1.1, "quiz description", 1, 2)
+        quiz_id = self.ctrl.add_quiz(b"s", "quiz description", 1, 2)
+
+    def test_crash_non_serializable(self):
+        """
+        Each of these conditions will cause a crash in the quizzes module due to an unexpected parameter that was passed. The passed parameter in these examples
+        is a bytes value and is not serializable by the json.dumps operation in save_data.
+        """
+        
+        quiz_id = self.ctrl.add_quiz("title", b"quiz description", 1, 2)
+        question_id = self.ctrl.add_question(quiz_id, "q_title", "q_body")  # setup
+        self.ctrl.add_answer(question_id, b"\x41", True)
 
 if __name__ == '__main__':
     unittest.main()
